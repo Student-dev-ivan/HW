97b50354c75f2c71d863abdbaf7d8b59d584b639
Ivan Bashmak <kiit15.bashmak@gmail.com>
2019-11-12 17:35:55 +0200

    Tic tac toe fix

---
+++ 5 task/scripts/nextVersion.js
@@ -0,0 +1,11 @@
+
+// function nextVersion(str) {
+//     const currentVersionNumbers = str.split('.');
+
+//     let currentVersionStr = numbers.join('');
+
+//     let nextVersion = Number(currentVersionStr) + 1;
+
+//     if(nextVersion.length )
+
+// }
--- 5 task/tic_tac_toe/index.js
+++ 5 task/tic_tac_toe/index.js
@@ -22,7 +22,7 @@ function isWon() {
         //Check digonals
         case boxArray[0] == boxArray[4] && boxArray[4] == boxArray[8] && boxArray[0] != -1:
         case boxArray[2] == boxArray[4] && boxArray[4] == boxArray[6] && boxArray[2] != -1:
-            alert(`Player ${turn == 1 ? "'X'" : "'O'"} won`);
+            setTimeout(() => alert(`Player ${turn == 1 ? "'X'" : "'O'"} won`), 500);
             init();
     }
 
